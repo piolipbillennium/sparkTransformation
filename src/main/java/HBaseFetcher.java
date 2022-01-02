18088d1f3b1963f28f3759660b12659d43f2d6c2
piolipbillennium <piotr.lipinski@billennium.com>
2022-01-02 19:19:16 +0100

    2 commit

--- src/main/java/HBaseFetcher.java
+++ src/main/java/HBaseFetcher.java
@@ -41,75 +41,40 @@ public class HBaseFetcher {
         config.addResource(new Path(properties.getProperty("hbase.site.path")));
 
         hBaseContext = new JavaHBaseContext(jsc, config);
-
+        System.out.println("11111111111111111111111111111111111111");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
         Dataset<Row> df2 = sparkSession.read()
                 .format("org.apache.hadoop.hbase.spark")
                 .option("hbase.columns.mapping",
                         "ROW String :key, " +
-                                "RAW_DATA_ID_city_name String RAW_DATA_ID:city_name," +
-                                "RAW_DATA_ID_longitude String RAW_DATA_ID:longitude," +
-                                "RAW_DATA_ID_latitude String RAW_DATA_ID:latitude," +
-                                "RAW_DATA_api_raw_data String RAW_DATA:api_raw_data")
+                                "RAW_DATA_ID:city_name String RAW_DATA_ID:city_name," +
+                                "RAW_DATA_ID:longitude String RAW_DATA_ID:longitude," +
+                                "RAW_DATA_ID:latitude String RAW_DATA_ID:latitude," +
+                                "RAW_DATA_api:raw_data String RAW_DATA:api_raw_data")
                 .option("hbase.table", "HOTEL_FETCHER_RAW_DATA")
                 .option("hbase.spark.use.hbasecontext", false)
                 .load();
-        df2.createOrReplaceTempView("personView");
-        //df2.printSchema();
-
+        df2.createOrReplaceTempView("data");
         df2.show();
-        //df2.select("RAW_DATA_444").show();
-        System.out.println("1");
+        //Dataset<Row> data = sparkSession.createDataFrame(df2);
+        System.out.println("11111111111111111111111111111111111111");
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
-        df2.getRows(1, 1);
-        System.out.println("2");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println(df2.getRows(1, 1));
-        System.out.println("3");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println(df2.getRows(1, 1).getClass());
-        System.out.println("4");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println(df2.getRows(1, 1).mkString());
-        System.out.println("5");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println(Arrays.toString(new Seq[]{df2.getRows(1, 1)}));
-        System.out.println("5");
         System.out.println();
         System.out.println();
         System.out.println();
         System.out.println();
 
-        //df2.select(col("RAW_DATA_333"), wheren(col("RAW_DATA_111").equalTo("London"))).show();
+        //Dataset<Row> col1 = sparkSession.sql("SELECT RAW_DATA_api_raw_data FROM data");
+        //col1.show();
 
-
-        /*sparkSession.sql("select RAW_DATA_333, RAW_DATA_111 " +
-                                "FROM personView " +
-                                "WHERE RAW_DATA_111 = 'London';").show();*/
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        //df2.select("RAW_DATA_333")
-        //System.out.println(df2.select(col("RAW_DATA_333"), when(col("RAW_DATA_111"). equalTo("London")),col("enabled")));
-        System.out.println("5");
-        //System.out.println(sparkSession.sql("select id,name,color, case when color = 'red' and price is not null then (price + 2.55) when color = 'red' and price is null then 2.55 else price end as price, enabled from df").show(););
-        System.out.println();
-        System.out.println();
-        System.out.println();
 
 
 
